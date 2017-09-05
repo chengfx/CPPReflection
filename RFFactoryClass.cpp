cfb0bcf4e6f68215040cb9d28ad07c39aaf723f6
flyercheng <CFX674939613@gmail.com>
2017-09-05 11:37:23 +0800

    Modify facotry file name

---
+++ RFFactoryClass.cpp
@@ -0,0 +1,29 @@
+#include "RFFactoryClass.h"
+#include <string>
+#include <map>
+
+namespace reflection{
+
+  using namespace std;
+
+  void* RFFactoryClass::getClassByName(string className)
+  {
+    auto iter = m_classMap.find(className);
+    if(m_classMap.find(className) == m_classMap.end())
+      return NULL;
+    else
+      return iter->second();
+  }
+
+  void RFFactoryClass::registClass(string name, CreateFunctionPtr method)
+  {
+    m_classMap.insert(pair<string, CreateFunctionPtr>(name, method));
+  }
+
+  RFFactoryClass& RFFactoryClass::sharedFactoryClass()
+  {
+    static RFFactoryClass _sharedClassFactory;
+    return _sharedClassFactory;
+  }
+
+}
